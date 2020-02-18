0df92a6825cceacca06191ad9e4cbbbc0d1dd0a3
vladchimescu <vladhkim@gmail.com>
2020-02-18 17:12:51 +0100

    restructured Juputer/1-intro-image-processing.ipynb and added code for handling compound multichannel image series (TIFF series)

--- transform/basic.py
+++ transform/basic.py
@@ -36,7 +36,7 @@ def load_imgstack(fname):
 
 
 # function for reading in TIFF images
-def read_tiff(fname):
+def read_tiff(fname, verbose=True):
     imgmd = get_img_metadata(fname)
     rdr = bf.ImageReader(fname, perform_init=True)
 
@@ -44,49 +44,25 @@ def read_tiff(fname):
                          imgmd.Pixels.get_SizeY()))
     imgarray = rdr.read()
 
-    print("Image size: %3d x %3d" % imgarray.shape)
+    if verbose:
+        print("Image size: %3d x %3d" % imgarray.shape)
 
     return imgarray
 
 
-def load_image_series(path, well):
-    tif_files = [f for f in os.listdir(path)
-                 if re.match('.*\.tiff', f)]
-    p = "^" + well + ".+$"
-    pos_list = [re.search(p, f).group() for f in tif_files
-                if re.search(well, f) is not None]
+def load_image_series(path, imgfiles):
+    imgmd = get_img_metadata(os.path.join(path, imgfiles[0]))
 
-    imgmd = get_img_metadata(path + tif_files[0])
+    imgarray = np.zeros((len(imgfiles),
+                         imgmd.Pixels.get_SizeX(),
+                         imgmd.Pixels.get_SizeY()))
 
-    # get the num of all the fields of view, z-stacks and color channels
-    n_fields = len(set([re.search('f\d+', f).group() for f in pos_list
-                        if re.search(well, f) is not None]))
-    n_stacks = len(set([re.search('p\d+', f).group() for f in pos_list
-                        if re.search(well, f) is not None]))
-    n_channels = len(set([re.search('ch\d+', f).group() for f in pos_list
-                          if re.search(well, f) is not None]))
+    for i, fname in enumerate(imgfiles):
+        imgarray[i, :, :] = read_tiff(os.path.join(path, fname),
+                                      verbose=False)
 
-    img_list = []
-    for well_pos in range(n_fields):
-        imgarray = np.zeros((n_stacks,
-                             imgmd.Pixels.get_SizeX(),
-                             imgmd.Pixels.get_SizeY(),
-                             n_channels))
-        for z in range(n_stacks):
-            for c in range(n_channels):
-                z_pat = "p"+str(z+1).zfill(2) + "-ch"
-                # pattern for slice z and color channel c
-                p = "r\d+c\d+f" + str(well_pos+1).zfill(2) + \
-                    z_pat + str(c+1) + ".+$"
-                fname = [re.search(p, i).group()
-                         for i in pos_list
-                         if re.search(p, i) is not None]
-                if len(fname) is not 0:
-                    imgarray[z, :, :, c] = read_tiff(path + fname[0])
-
-        img_list.append(imgarray)
-
-    return img_list
+    print("Series of %d images of size: %3d x %3d" % imgarray.shape)
+    return imgarray
 
 
 # write TIFF series (image stack, multichannel)
