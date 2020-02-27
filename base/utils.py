e8e9a22e900bdb497424cb1f75896d8e3793d145
vladchimescu <vladhkim@gmail.com>
2020-02-27 17:30:36 +0100

    moved _internals.py to base.utils

--- base/utils.py
+++ base/utils.py
@@ -7,8 +7,39 @@ import os
 import numpy as np
 import bioformats as bf
 from skimage import img_as_uint
+import re
+import xml.etree.cElementTree as ET
 
-from _internals import get_img_metadata
+
+# image metadata using Bioformats OME XML parser
+def get_img_metadata(fname):
+    omexml = bf.get_omexml_metadata(fname)
+    md = bf.OMEXML(omexml)
+    imgmd = md.image()
+    return imgmd
+
+# get physical scale in Z-axis direction
+
+
+def get_physicalSizeZ(fname):
+    omexml = bf.get_omexml_metadata(fname)
+    xml = bytes(bytearray(omexml, encoding='utf-8'))
+    root = ET.fromstring(xml)
+
+    for child in root.iter():
+        if re.search("Pixels", child.tag) is not None:
+            scaleZ = child.attrib['PhysicalSizeZ']
+    return np.float64(scaleZ)
+
+# return physical scale in all directions (ZXY)
+
+
+def get_physical_scale(fname):
+    imgmd = get_img_metadata(fname)
+    physicalSizeZ = get_physicalSizeZ(fname)
+
+    return [physicalSizeZ, imgmd.Pixels.get_PhysicalSizeX(),
+            imgmd.Pixels.get_PhysicalSizeY()]
 
 
 # function for reading in .czi images
