d965e445e509b7f3eec2ee8aa399c98efbb6884b
vladchimescu <vladhkim@gmail.com>
2020-04-23 12:59:20 +0200

    added documentation for segment.tools

--- segment/tools.py
+++ segment/tools.py
@@ -3,6 +3,30 @@ import numpy as np
 
 
 def make_bbox(feats, rmax, cmax, pad):
+    '''Make bounding boxes for labelled regions
+       ----------------------------------------
+       Make a list of bounding box coordinates from
+       region properties list or np.array
+
+       Parameters
+       ----------
+       feats : np.array or list
+           List of RegionProperties or np.array with
+           bounding box coordinates
+       rmax : int
+           Maximum height of an image
+       cmax : int
+           Maximum width of an image
+       pad : int
+           Padding value for the bounding boxes
+
+       Returns
+       -------
+       bbox : list
+           List of bounding box coordinates in
+           (xmin, xmax, ymin, ymax) tuples
+
+    '''
     bbox = []
     for i in range(len(feats)):
         if type(feats) == np.ndarray:
@@ -10,14 +34,36 @@ def make_bbox(feats, rmax, cmax, pad):
         elif type(feats) == list:
             ymin, xmin, ymax, xmax = feats[i].bbox
         bb = np.array((max(0, xmin - pad),
-                       min(xmax + pad, rmax - 1),
+                       min(xmax + pad, cmax - 1),
                        max(0, ymin - pad),
-                       min(ymax + pad, cmax - 1)))
+                       min(ymax + pad, rmax - 1)))
         bbox.append(bb)
     return bbox
 
 
 def read_bbox(df, rmax, cmax, pad=20):
+    '''Extract bounding boxes from DataFrame
+       -------------------------------------
+       Make a list of bounding box coordinate tuples
+       from the region properties DataFrame
+
+       Parameters
+       ----------
+       df : DataFrame
+           DataFrame of RegionProperties
+       rmax : int
+           Maximum height of an image
+       cmax : int
+           Maximum width of an image
+       pad : int
+           Padding value for the bounding boxes
+
+       Returns
+       -------
+       bbox : list
+           List of bounding box coordinates in
+           (xmin, xmax, ymin, ymax) tuples
+    '''
     bbox_array = df[['bbox-0', 'bbox-1', 'bbox-2', 'bbox-3']].values
 
     bbox = make_bbox(feats=bbox_array,
