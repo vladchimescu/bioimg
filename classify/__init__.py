4f8f4911d20a651a3298378ed235f51de0155b28
vladchimescu <vladhkim@gmail.com>
2020-05-12 16:10:12 +0200

    finished IncrementalClassifier (in essence) and put it in random_forest.py

--- classify/__init__.py
+++ classify/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from .generic import ImgX, glcm_to_dataframe, compute_region_props
+from .random_forest import IncrementalClassifier
 
 __all__ = ['ImgX', 'glcm_to_dataframe',
-           'compute_region_props']
+           'compute_region_props',
+           'IncrementalClassifier']
--- classify/random_forest.py
+++ classify/random_forest.py
@@ -3,90 +3,67 @@
 Image classification with random forest
 """
 import numpy as np
-from plotly.offline import iplot
-from skimage.exposure import equalize_adapthist
-from skimage.transform import resize
-from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import re
+import h5py
+import pandas as pd
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import cross_val_score
-import matplotlib.pyplot as plt
-import h5py
+from plotly.offline import iplot
+from ..extra.viz import plotly_viz, plotly_predictions
 
 
 class IncrementalClassifier:
-    def __init__(self, **kwargs):
+    def __init__(self):
         # initialize with 'None' something to be loaded later
-        self.img = None
-        self.bb = None
+        self.imgx = None
+
         self.newlabels = None
+        # training data
+        self.Xtrain = None
+        self.ytrain = None
         # inialize classifier as 'None'
         self.clf = None
-        self.pca = None
+        self.classes = None
 
     def __setattr__(self, name, value):
         self.__dict__[name] = value
+        # if a new ImgX object is passed,
+        # compute its features
+        if name == 'imgx':
+            self._compute_imgx_data()
+            self.newlabels = None
 
     # function for setting individual class parameters
     def set_param(self, **kwargs):
         for k in kwargs.keys():
             self.__setattr__(k, kwargs[k])
 
-    # generate predictions with the loaded feature data
-    def generate_predictions(self, prob=False):
-        if self.X_test_norm is None:
-            cellbb_norm = [resize(cb, (self.w, self.h), anti_aliasing=True)
-                           for cb in self.cellbb]
-            self.X_test_norm = np.array([cbn.ravel() for cbn in cellbb_norm])
-
-        if self.X_test_prop is None:
-            X_prop_list = [get_regionprop_feats(
-                mip_rgb=cbb, exclude=exclude) for cbb in self.cellbb]
-            self.X_test_prop = np.vstack(X_prop_list)
-
-        X_test_pca = self.pca.transform(self.X_test_norm)
-        X_test_all = np.append(X_test_pca, self.X_test_prop, axis=1)
-
-        self.y_pred = self.clf.predict(X_test_all)
-        if prob:
-            self.y_prob = self.clf.predict_proba(X_test_all)
-        return self
-
-    # set plotly graphical layers
-    def set_scene(self):
-        # derived object attributes
-        wellpos = convert_well_name(self.select_well)
-
-        mip = fn.get_mip(path=self.path, wellpos=wellpos)
-        mip_rgb = equalize_adapthist(np.dstack((mip[:, :, 1],
-                                                mip[:, :, 2],
-                                                mip[:, :, 0])))
-        self.img = mip_rgb
-        if self.y_pred is not None:
-            self.layout, self.feats = vi.plotly_predictions(img=mip_rgb,
-                                                            bb=self.bb,
-                                                            y_pred=self.y_pred,
-                                                            target_names=self.target_names)
-        else:
-            self.layout, self.feats = vi.plotly_viz(img=mip_rgb,
-                                                    bb=self.bb)
-        return self
+    # internal function checks if the embedded
+    # imgx object has the features computed
+    def _compute_imgx_data(self):
+        if self.imgx is not None and len(self.imgx.data) == 0:
+            self.imgx.compute_props()
 
     # plot predictions overlaid with the original image
     # plot is a 'void' function (returns 'None')
-    def plot(self):
-        iplot(dict(data=self.feats, layout=self.layout))
-
-    # update scene after refitting the pipeline
-    def update_scene(self):
-        self.feats = vi.update_feats(img=self.img,
-                                     bb=self.bb,
-                                     y_pred=self.y_pred,
-                                     target_names=self.target_names)
-        return self
+    def plot_predictions(self):
+        if self.imgx.y is not None:
+            layout, feats = plotly_predictions(img=self.imgx.img,
+                                               bb=self.imgx.bbox,
+                                               ypred=self.imgx.y,
+                                               labels=self.classes
+                                               )
+        else:
+            layout, feats = plotly_viz(img=self.imgx.img,
+                                       bb=self.imgx.bbox)
+        iplot(dict(data=feats, layout=layout))
 
     def add_instances(self, newlabels):
+        newlabels = np.unique(newlabels, axis=0)
         if self.newlabels is None:
             self.newlabels = newlabels
         else:
@@ -95,83 +72,60 @@ class IncrementalClassifier:
             a2_rows = self.newlabels.view(
                 [('', self.newlabels.dtype)] * self.newlabels.shape[1])
 
-            self.newlabels = newlabels
             newlabels = (np.setdiff1d(a1_rows, a2_rows).
                          view(newlabels.dtype).
                          reshape(-1, newlabels.shape[1]))
-
-        if newlabels.size:
-            cellbb_new = [self.cellbb[i] for i in newlabels[:, 0]]
-            cellbb_new_norm = [resize(cb, (self.w, self.h), anti_aliasing=True)
-                               for cb in cellbb_new]
-            X_new_norm = np.array([cbn.ravel() for cbn in cellbb_new_norm])
-
-            self.X_train_norm = np.concatenate((self.X_train_norm, X_new_norm))
-            self.y_train = np.append(self.y_train, newlabels[:, 1])
-
-            X_prop_list = [get_regionprop_feats(
-                mip_rgb=cbb, exclude=exclude) for cbb in cellbb_new]
-            X_new_prop = np.vstack(X_prop_list)
-            self.X_train_prop = np.concatenate((self.X_train_prop, X_new_prop))
+            self.newlabels = np.append(self.newlabels, newlabels, axis=0)
+        # if 'newlabels' array is not empty
+        if len(newlabels) > 0:
+            self._push_traindata(newlabels=newlabels)
         return self
 
-    def train_classifier(self, n_components=150,
-                         bootstrap=True,
-                         max_depth=None,
-                         n_estimators=100,
-                         max_features='sqrt',
-                         min_samples_split=5,
-                         min_samples_leaf=5,
-                         random_state=100):
+    def _push_traindata(self, newlabels):
+        ids = newlabels[:, 0]
+        if self.Xtrain is None:
+            self.Xtrain = self.imgx.data.iloc[ids, :]
+            self.ytrain = label_binarize(newlabels[:, 1],
+                                         classes=range(len(self.classes)))
+        else:
+            self.Xtrain = pd.concat(
+                [self.Xtrain, self.imgx.data.iloc[ids, :]], axis=0)
+            self.ytrain = np.append(self.ytrain, label_binarize(newlabels[:, 1],
+                                                                classes=range(len(self.classes))), axis=0)
 
-        self.pca = PCA(n_components=n_components, svd_solver='randomized',
-                       whiten=True, random_state=random_state).fit(self.X_train_norm)
+    def set_classifier(self, clf=None):
+        self.clf = clf
+        # if 'None' then some reasonable default
+        if clf is None:
+            self.clf = OneVsRestClassifier(RandomForestClassifier(bootstrap=True,
+                                                                  class_weight="balanced",
+                                                                  n_estimators=500,
+                                                                  random_state=123,
+                                                                  n_jobs=-1))
+        return self
 
-        # project the train data
-        X_train_pca = self.pca.transform(self.X_train_norm)
-        X_train_all = np.append(X_train_pca, self.X_train_prop, axis=1)
-
-        self.clf = RandomForestClassifier(bootstrap=bootstrap,
-                                          class_weight="balanced",
-                                          max_depth=max_depth,
-                                          n_estimators=n_estimators,
-                                          max_features=max_features,
-                                          min_samples_split=min_samples_split,
-                                          min_samples_leaf=min_samples_leaf,
-                                          random_state=random_state,
-                                          n_jobs=-1)
-
-        self.clf.fit(X_train_all, self.y_train)
+    def train_classifier(self):
+        self.clf.fit(self.Xtrain, self.ytrain)
         return self
 
     # print the confusion matrix on the existing training set
-    def get_classifiction_report(self):
-        X_train_pca = self.pca.transform(self.X_train_norm)
-        X_train_all = np.append(X_train_pca, self.X_train_prop, axis=1)
-        y_pred = self.clf.predict(X_train_all)
-        print(classification_report(self.y_train,
-                                    y_pred, target_names=self.target_names))
-        print(confusion_matrix(self.y_train, y_pred,
-                               labels=range(len(self.target_names))))
-        return self
+    def train_error(self):
+        ypred = self.clf.predict(self.Xtrain)
+        print(classification_report(self.ytrain.argmax(axis=1),
+                                    ypred.argmax(axis=1),
+                                    target_names=self.classes))
+        # print(confusion_matrix(self.ytrain.argmax(axis=1), ypred.argmax(axis=1),
+        #                       labels=range(len(self.classes))))
 
-    def get_cross_val_score(self, kfold=5):
-        X_train_pca = self.pca.transform(self.X_train_norm)
-        X_train_all = np.append(X_train_pca, self.X_train_prop, axis=1)
-        scores = cross_val_score(self.clf, X_train_all, self.y_train, cv=kfold)
-        print scores
-
-        return self
-
-    def reset(self):
-        self.newlabels = None
-        self.X_test_norm = None
-        self.X_test_prop = None
-        return self
+    # generate predictions and pass them to self.imgx.y
+    def generate_predictions(self, prob=False):
+        Xtest = self.imgx.data
+        ypred = self.clf.predict(Xtest)
+        # set labels to these
+        self.imgx.y = ypred.argmax(axis=1)
 
     def h5_write(self, fname):
         hf = h5py.File(fname, 'w')
-        hf.create_dataset('X_train_norm', data=self.X_train_norm)
-        hf.create_dataset('X_train_prop', data=self.X_train_prop)
-        hf.create_dataset('y_train', data=self.y_train)
+        hf.create_dataset('Xtrain', data=self.Xtrain)
+        hf.create_dataset('ytrain', data=self.ytrain.argmax(axis=1))
         hf.close()
