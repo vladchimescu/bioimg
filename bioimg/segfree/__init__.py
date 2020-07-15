dd90c77ff23500f5cb28481b1cd2a227a2c2aecf
vladchimescu <vladhkim@gmail.com>
2020-07-15 13:20:30 +0200

    SegfreeProfiler class: added fit-transform methods

--- bioimg/segfree/__init__.py
+++ bioimg/segfree/__init__.py
@@ -1,1 +1,4 @@
 #!/usr/bin/env python3
+from .profile import SegfreeProfiler
+
+__all__ = ['SegfreeProfiler']
--- bioimg/segfree/profile.py
+++ bioimg/segfree/profile.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from skimage.util import view_as_blocks, view_as_windows
+from skimage.util import img_as_ubyte
 
 def get_blocktype(a, nbits):
     block_type = {i : np.sum(a == i) / a.size for i in nbits}
@@ -14,21 +15,25 @@ def get_blockfeats(img_tiles, nbits):
 def get_supblocks(bf, km_block, grid_shape, window_shape=3):
     img_blocked = km_block.predict(bf).reshape(grid_shape)
     supblocks = view_as_windows(img_blocked, window_shape=window_shape).reshape(-1,window_shape,window_shape)
-    return pd.concat([get_blocktype(supblocks[i], nbits=50) for i in range(supblocks.shape[0])])
+    # correct here: instead of range(50) -> n_supblock_types
+    return pd.concat([get_blocktype(supblocks[i], nbits=range(50)) for i in range(supblocks.shape[0])])
 
 class SegfreeProfiler:
-    def __init__(self):
+    def __init__(self, **kwargs):
         '''
         Parameters
         ----------
-        imgs : list of images
         tile_size : tuple
+        km_pixel : KMeans object for pixels (color compression)
         km_block : KMeans object for tiles (blocks)
         km_supblock : KMeans object for superblocks
         '''
-        self.imgs = None
-        self.blockfeats = None
-        self.tile_size = None
+        self.tile_size = kwargs.get('tile_size', None)
+        self.n_block_types = kwargs.get('n_block_types', 50)
+        self.n_supblock_types = kwargs.get('n_supblock_types', 30)
+        
+        # these are initialized with 'None'
+        self.km_pixel = None
         self.km_block = None
         self.km_supblock = None
 
@@ -39,22 +44,34 @@ class SegfreeProfiler:
         for k in kwargs.keys():
             self.__setattr__(k, kwargs[k])
 
-    def preprocess_images(self, thresh=True):
-        if thresh:
-            self.imgs = [threshold_img(img, method='otsu') for img in self.imgs]
-        self.imgs = [img_as_ubyte(img) for img in self.imgs]
+    def tile_images(self, imgs):
+        return [view_as_blocks(img,
+                               block_shape=self.tile_size).reshape(-1, *self.tile_size) for img in imgs]        
 
-    def tile_images(self):
-        return [view_as_blocks(img, block_shape=self.tile_size) for img in self.imgs] 
-       
-
-    def fit(self, n_clusters=50, n_init=50, random_state=1307):
-        img_tiles = self.tile_images()
-        nbits = np.unique(np.stack(self.blocks))
-        self.blockfeats = [get_blockfeats(t, nbits=nbits) for t in img_tiles]
-        self.km_block = KMeans(n_clusters=n_clusters,
+    def fit(self, imgs, n_init=50, random_state=1307):
+        img_tiles = self.tile_images(imgs)
+        nbits = np.unique(np.stack(img_tiles))
+        blockfeats = [get_blockfeats(t, nbits=nbits) for t in img_tiles]
+        self.km_block = KMeans(n_clusters=self.n_block_types,
                                n_init=n_init,
-                               random_state=random_state).fit(pd.concat(self.blockfeats))
+                               random_state=random_state).fit(pd.concat(blockfeats))
+        grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape, self.tile_size))
+        supblocks = [get_supblocks(bf,
+                                   km_block=self.km_block,
+                                   grid_shape=grid_shape) for bf in blockfeats]
+        self.km_supblock = KMeans(n_clusters=self.n_supblock_types,
+                                  n_init=n_init,
+                                  random_state=random_state).fit(pd.concat(supblocks))
+        return self
 
-    def transform(self, newimgs):
-        pass
+    def transform(self, imgs):
+        img_tiles = self.tile_images(imgs)
+        nbits = np.unique(np.stack(img_tiles))
+        blockfeats = [get_blockfeats(t, nbits=nbits) for t in img_tiles]
+        grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape, self.tile_size))
+        supblocks = [get_supblocks(bf,
+                                   km_block=self.km_block,
+                                   grid_shape=grid_shape) for bf in blockfeats]
+        img_profs = pd.concat([get_blocktype(self.km_supblock.predict(supblocks[i]),
+                                             nbits=range(self.n_supblock_types)) for i in range(len(supblocks))]).reset_index(drop=True)
+        return img_profs
