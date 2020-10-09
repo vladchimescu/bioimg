b4f686179a12a17effa8470e54cecdd8787f96a7
vladchimescu <vladhkim@gmail.com>
2020-10-09 11:00:45 +0200

    streamlined segfree.profile module

--- bioimg/segfree/profile.py
+++ bioimg/segfree/profile.py
@@ -69,7 +69,7 @@ def get_block_counts(a):
     block_type = dict(zip(*np.unique(a, return_counts=True)))
     return pd.DataFrame([block_type]) / a.size
 
-def get_blockfeats(blocks):
+def get_greyscale_blockfeats(blocks):
     '''Compute features for greyscale image blocks
        -------------------------------------------
        For greyscale blocks, the features are 
@@ -123,15 +123,17 @@ def get_block_types(bf, km_block, cols, grid_shape):
     img_blocked = img_blocked.reshape(grid_shape)
     return img_blocked
 
-def get_supblocks(img_blocked, window_shape=3):
-    '''Computes superblock features for greyscale images
-       -------------------------------------------------
+def get_supblocks(img_blocked, thresh, window_shape=3):
+    '''Computes superblock features for both greyscale/multichannel images
+       -------------------------------------------------------------------
 
        Parameters
        ----------
        img_blocked : array
            Grid (matrix) with block types
            (Block types are integers in range 1 ... n_block_types)
+       thresh : bool
+           Indicator of whether the images were thresholded or not
        window_shape : int (optional)
            Size of a sliding window, by default 3x3 window is used
 
@@ -146,12 +148,9 @@ def get_supblocks(img_blocked, window_shape=3):
                                 window_shape=window_shape).reshape(-1,window_shape,
                                                                    window_shape)
     mid = np.ceil(window_shape/2).astype(int) - 1
-    fgr_supblocks = np.stack([sb for sb in supblocks if sb[mid,mid]])
-    return pd.concat([get_block_counts(sb) for sb in fgr_supblocks])
-
-def get_color_supblocks(img, window_shape=3):
-    supblocks = view_as_windows(img,
-                                    window_shape=window_shape).reshape(-1,window_shape, window_shape)
+    if thresh:
+        fgr_supblocks = np.stack([sb for sb in supblocks if sb[mid,mid]])
+        return pd.concat([get_block_counts(sb) for sb in fgr_supblocks])
     return pd.concat([get_block_counts(sb) for sb in supblocks])
 
 def flatten_tiles(blocks):
@@ -169,9 +168,12 @@ class SegfreeProfiler:
         tile_size : tuple
         n_block_types : int
         n_supblock_types : int
-        n_components : int
         n_subset : int
-        pca : PCA object for dimensionality reduction
+        thresh : bool
+        
+        colors : int
+        pixel_types : int
+        
         km_block : KMeans object for tiles (blocks)
         km_supblock : KMeans object for superblocks
 
@@ -185,14 +187,16 @@ class SegfreeProfiler:
         self.tile_size = kwargs.get('tile_size', None)
         self.n_block_types = kwargs.get('n_block_types', 50)
         self.n_supblock_types = kwargs.get('n_supblock_types', 30)
-        self.n_components = kwargs.get('n_components', 50)
         self.n_subset = kwargs.get('n_subset', 10000)
+        self.thresh = True
         
-        # these are initialized with 'None'
-        self.pca = None
+        self.colors = 10
+        self.pixel_types = None
+        
         self.km_block = None
         self.km_supblock = None
-        self.pixel_types = None
+        
+        self.cache = dict()
 
     def __setattr__(self, name, value):
         self.__dict__[name] = value
@@ -200,172 +204,124 @@ class SegfreeProfiler:
     def set_param(self, **kwargs):
         for k in kwargs.keys():
             self.__setattr__(k, kwargs[k])
-
-    def _fit_single_channel(self, imgs, n_init,
-                            random_state,
-                            downsample,
-                            transform):
-        img_tiles = tile_images(imgs, self.tile_size)
-        print("Estimating tile properties")
-        blockfeats = [get_blockfeats(t) for t in img_tiles]
+            
+    def _handle_greyscale(self, imgs=imgs):
+        # threshold, convert to ubyte,
+        # bin the greyscale levels and compute tile features
+        if self.thresh:
+            imgs = [threshold_img(img, method='otsu') for img in imgs]
+        imgs = [img_as_ubyte(img) for img in imgs]
+        tiled_imgs = tile_images(imgs, self.tile_size)
+        # total number of image tiles
+        n_tiles = len(img_tiles) * tiled_imgs[0].shape[0]
+        # bin greyscale levels for each image
+        blockfeats = [get_greyscale_blockfeats(t) for t in tiled_imgs]
+        return blockfeats, n_tiles
+    
+    # finish handle_multichannel function
+    def _handle_multichannel(self, imgs=imgs):
+        if self.thresh:
+            imgs = threshold_multichannel(imgs)
+        pass
+        # preprocess: threshold, compress the colors,
+        # and compute tile features
+        
+    def _kmeans_tiles(self, blockdf, n_init, random_state, downsample):
+        # downsample the image tiles
+        subset = range(blockdf.shape[0])
+        if downsample and self.n_subset < blockdf.shape[0]:
+            np.random.seed(random_state)
+            subset = np.random.choice(range(blockdf.shape[0]), size=self.n_subset)
+        print("Running k-means on tiles")
+        self.km_block = KMeans(n_clusters=self.n_block_types,
+                               n_init=n_init,
+                               random_state=random_state).fit(blockdf.iloc[subset,:])
+            
+    def fit(self, imgs, n_init=50,
+            random_state=1307,
+            downsample=True):
+        if imgs[0].ndim == 2:
+            print("Fitting model for greyscale images")
+            blockfeats, n_tiles = self._handle_greyscale(imgs=imgs)
+        if imgs[0].ndim == 3:
+            print("Fitting model for multichannel images")
+            blockfeats, n_tiles = self._handle_multichannel(imgs=imgs)
         blockdf = pd.concat(blockfeats).fillna(0)
         self.pixel_types = blockdf.columns.values
         
-        print("Running k-means on tiles")
-        subset = range(blockdf.shape[0])
-        # downsample the image tiles
-        if downsample:
-            np.random.seed(random_state)
-            subset = np.random.choice(range(blockdf.shape[0]), size=self.n_subset)
-        self.km_block = KMeans(n_clusters=self.n_block_types,
-                               n_init=n_init,
-                               random_state=random_state).fit(blockdf.loc[subset,:])
+        # run kmeans on blocks
+        self._kmeans_tiles(blockdf=blockdf, 
+                           n_init=n_init, 
+                           random_state=random_state,
+                           downsample=downsample)
         
-        grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape, self.tile_size))
-        blocks = [get_block_types(bf,
+        grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape,
+                                                     self.tile_size))
+        tile_labels = [get_block_types(bf,
                               km_block=self.km_block,
                               cols=self.pixel_types,
                               grid_shape=grid_shape) for bf in blockfeats]
-        supblocks = [get_supblocks(bl) for bl in blocks]
+        supblocks = [get_supblocks(bl, thresh=True) for bl in tile_labels]
+        
         print("Running k-means on superblocks")
         self.km_supblock = KMeans(n_clusters=self.n_supblock_types,
                                   n_init=n_init,
                                   random_state=random_state).fit(pd.concat(supblocks).fillna(0))
-        if transform:
-            return self._transform_single_channel(imgs, blocks, supblocks)
-        print("Done")
-
-    def _fit_multichannel(self, imgs, n_init,
-                          random_state,
-                          downsample,
-                          transform):
-        img_tiles = tile_images(imgs, self.tile_size)
-        Xtrain = np.concatenate([flatten_tiles(t) for t in img_tiles])
-        print("Running PCA on tiles")
-        self.pca = PCA(n_components=self.n_components,
-                       svd_solver='randomized',
-                       whiten=True,
-                       random_state=random_state).fit(Xtrain)
-        blockdf = self.pca.transform(Xtrain)
-
-        print("Running k-means on tiles")
-        subset = range(blockdf.shape[0])
-        # downsample the image tiles
-        if downsample:
-            np.random.seed(random_state)
-            subset = np.random.choice(range(blockdf.shape[0]), size=self.n_subset)
-        self.km_block = KMeans(n_clusters=self.n_block_types,
-                               n_init=n_init,
-                               random_state=random_state).fit(blockdf[subset,:])
-
-        grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape, self.tile_size))
-        img_blocked = self.km_block.predict(blockdf).reshape(-1, *grid_shape)
-        supblocks = [get_color_supblocks(img_blocked[i]) for i in range(img_blocked.shape[0])]
-        print("Running k-means on superblocks")
-        self.km_supblock = KMeans(n_clusters=self.n_supblock_types,
-                                  n_init=n_init,
-                                  random_state=random_state).fit(pd.concat(supblocks).fillna(0))
-        if transform:
-            return self._transform_multichannel(imgs, img_blocked, supblocks)
-        print("Done")
-        
-        
-
-    def fit(self, imgs, n_init=50,
-            random_state=1307,
-            downsample=True,
-            transform=False):
-        if imgs[0].ndim == 2:
-            print("Fitting model for greyscale images")
-            self._fit_single_channel(imgs=imgs, n_init=n_init,
-                                     random_state=random_state,
-                                     downsample=downsample,
-                                     transform=transform)
-        if imgs[0].ndim == 3:
-            print("Fitting model for multichannel images")
-            self._fit_multichannel(imgs=imgs, n_init=n_init,
-                                   random_state=random_state,
-                                   downsample=downsample,
-                                   transform=transform)
+        # cache blocks and supblocks
+        self.cache['blocks'] = tile_labels
+        self.cache['supblocks'] = supblocks
         return self
-
-    def fit_transform(self, imgs, n_init=50,
-                      random_state=1307,
-                      downsample=True,
-                      transform=True):
-        if imgs[0].ndim == 2:
-            print("Fitting model for greyscale images")
-            return self._fit_single_channel(imgs=imgs,
-                                     n_init=n_init,
-                                     random_state=random_state,
-                                     downsample=downsample,
-                                     transform=transform)
-        if imgs[0].ndim == 3:
-            print("Fitting model for multichannel images")
-            return self._fit_multichannel(imgs=imgs,
-                                          n_init=n_init,
-                                          random_state=random_state,
-                                          downsample=downsample,
-                                          transform=transform)
-
-    def _transform_single_channel(self, imgs,
-                                  blocks=None,
-                                  supblocks=None):
-        img_tiles = tile_images(imgs, self.tile_size)           
-        pixel_mean = (pd.concat([get_block_counts(t) for t in img_tiles]).
-                      reindex(columns=self.pixel_types).
-                      fillna(0).
-                      reset_index(drop=True))
-        pixel_mean.columns = ['-'.join(['pixel', str(col)])
-                           for col in pixel_mean.columns.values]
-        grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape, self.tile_size))
-        if supblocks is None:
-            blockfeats = [get_blockfeats(t) for t in img_tiles]
-            blockdf = pd.concat(blockfeats).fillna(0)
-            blocks = [get_block_types(bf,
-                              km_block=self.km_block,
-                              cols=self.pixel_types,
-                              grid_shape=grid_shape) for bf in blockfeats]
-            supblocks = [get_supblocks(bl) for bl in blocks]
-        block_mean = pd.concat([get_block_counts(bl).reindex(columns=range(self.n_block_types+1)) for bl in blocks]).fillna(0).reset_index(drop=True)
+    
+    def _get_mean_tile(self, blocks):
+        columns = range(self.n_block_types+1)
+        block_mean = pd.concat([get_block_counts(bl).reindex(columns=columns)
+                                for bl in blocks]).fillna(0).reset_index(drop=True)
         block_mean.columns = ['-'.join(['block', str(col)])
                            for col in block_mean.columns.values]
-        supblock_mean = pd.concat([get_block_counts(self.km_supblock.predict(sbf.reindex(columns=range(self.n_block_types + 1)).fillna(0)))
+        return block_mean
+    
+    def _get_mean_superblock(self, supblocks):
+        columns = range(self.n_block_types + 1)
+        # re-index the feature columns of every superblock
+        # so that it matches the number of block types
+        supblocks = [sbf.reindex(columns=columns).fillna(0) for sbf in supblocks]
+        supblock_mean = pd.concat([get_block_counts(self.km_supblock.predict(sbf))
                        for sbf in supblocks]).reset_index(drop=True)
         supblock_mean = supblock_mean.reindex(columns=range(self.n_supblock_types)).fillna(0)
         supblock_mean.columns = ['-'.join(['superblock', str(col+1)])
            for col in supblock_mean.columns.values]
-        img_prof = pd.concat([supblock_mean, block_mean,  pixel_mean], axis=1)
+        return supblock_mean
+    
+    def transform(self, imgs, useCache=False):
+        if useCache:
+            blocks = self.cache['blocks']
+            supblocks = self.cache['supblocks']
+        else:
+            if imgs[0].ndim == 2:
+                blockfeats, n_tiles = self._handle_greyscale(imgs=imgs)
+            if imgs[0].ndim == 3:
+                blockfeats, n_tiles = self._handle_multichannel(imgs=imgs)
+            blockdf = pd.concat(blockfeats).fillna(0)
+
+            grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape,
+                                                         self.tile_size))
+            blocks = [get_block_types(bf,
+                                  km_block=self.km_block,
+                                  cols=self.pixel_types,
+                                  grid_shape=grid_shape) for bf in blockfeats]
+            supblocks = [get_supblocks(bl, thresh=True) for bl in blocks]
+        # get proportion of every tile type in each image
+        block_mean = self._get_mean_tile(blocks)
+        # get proprotions of superblock types in each image
+        supblock_mean = self._get_mean_superblock(supblocks)
+        # join block and superblock profiles
+        img_prof = pd.concat([supblock_mean, block_mean], axis=1)
         return img_prof
-
-    def _transform_multichannel(self, imgs,
-                                img_blocked=None,
-                                supblocks=None):
-        img_tiles = tile_images(imgs, self.tile_size)
-        Xtest = np.concatenate([flatten_tiles(t) for t in img_tiles])
-        blockdf = self.pca.transform(Xtest)
-        pc_mean = pd.DataFrame(blockdf,
-                               columns=['PC'+ str(i+1) for i in range(self.n_components)])
-        pc_mean.index = np.repeat(range(len(img_tiles)), img_tiles[0].shape[0])
-        pc_mean = pc_mean.groupby(pc_mean.index).agg('mean')
-        if supblocks is None:
-            grid_shape = tuple(int(x / y) for x,y in zip(imgs[0].shape, self.tile_size))
-            img_blocked = self.km_block.predict(blockdf).reshape(-1, *grid_shape)
-            supblocks = [get_color_supblocks(img_blocked[i]) for i in range(img_blocked.shape[0])]
-        block_mean = pd.concat([get_block_counts(img_blocked[i]).reindex(columns=range(self.n_block_types)) for i in range(img_blocked.shape[0])]).fillna(0).reset_index(drop=True)
-        block_mean.columns = ['-'.join(['block', str(col+1)])
-                           for col in block_mean.columns.values]
-        supblock_mean = pd.concat([get_block_counts(self.km_supblock.predict(sbf.reindex(columns=range(self.n_block_types)).fillna(0)))
-                       for sbf in supblocks]).reset_index(drop=True)
-        supblock_mean = supblock_mean.reindex(columns=range(self.n_supblock_types)).fillna(0)
-        supblock_mean.columns = ['-'.join(['superblock', str(col+1)])
-           for col in supblock_mean.columns.values]
-        img_prof = pd.concat([supblock_mean, block_mean, pc_mean], axis=1)
-        return img_prof
-
-
-    def transform(self, imgs):
-        if imgs[0].ndim == 2:
-            return self._transform_single_channel(imgs)
-        if imgs[0].ndim == 3:
-            return self._transform_multichannel(imgs)
+    
+    def fit_transform(self, imgs, n_init=50,
+                      random_state=1307,
+                      downsample=True):
+        return self.fit(imgs=imgs,
+                 n_init=n_init,
+                 random_state=random_state,
+                 downsample=downsample).transform(imgs=imgs, useCache=True)
